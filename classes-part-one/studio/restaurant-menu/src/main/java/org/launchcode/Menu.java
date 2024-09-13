4ef8bf313424493108acb8af2ea326dd8a23e615
Carolyn Smith <carolynsmith@slu.edu>
2024-09-12 19:35:15 -0500

    Studio

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    Date lastUpdated = new Date();
+    ArrayList<MenuItem> menuItems = new ArrayList<>();
+
+
+}
---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -0,0 +1,60 @@
+package org.launchcode;
+
+public class MenuItem {
+    private String name;
+    private float price;
+    private String description;
+    private String category;
+    private boolean isNew;
+
+    public MenuItem() {String name, float price, String description, String category, boolean IsNew) {
+
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+}
