90ec9566f07661aa15ee6c850e0a69076a8e705c
Carolyn Smith <carolynsmith@slu.edu>
2024-09-24 22:34:57 -0500

    updated

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/ReadFile.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/ReadFile.java
@@ -1,4 +1,12 @@
 package org.launchcode;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReadFile {
+    public static void main(String[] args) {
+
+        String filePath = "path/file.txt";
+    }
 }
